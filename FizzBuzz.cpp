0b9ee641c6eaddab847e6dd6aba1493b890710eb
SpGit8 <spandana.v8@gmail.com>
2017-03-24 01:44:23 -0400

    Update FizzBuzz.cpp

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -2,7 +2,10 @@
 Write a program that outputs the string representation of numbers from 1 to n.
 
 But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. 
-For numbers which are multiples of both three and five output “FizzBuzz”.*/
+For numbers which are multiples of both three and five output “FizzBuzz”.
+*/
+using namespace std;
+
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
